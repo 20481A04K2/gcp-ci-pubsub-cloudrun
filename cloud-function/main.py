33b16e2e83af6a703582d88cf1a0d0e1cf4dd6f5
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-07-23 10:49:58 +0530

    Create main.py

---
+++ cloud-function/main.py
@@ -0,0 +1,11 @@
+import base64
+import json
+from deploy_to_run import deploy_to_cloud_run
+
+def deploy_app_logic(event, context):
+    message = base64.b64decode(event['data']).decode('utf-8')
+    data = json.loads(message)
+    image = data.get("image")
+
+    if image:
+        deploy_to_cloud_run("my-app", image)
