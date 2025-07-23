9707921f2498d3b5774721b8c353567e342cac69
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-07-23 14:05:08 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello from Cloud Run via Pub/Sub + Cloud Function!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
